954bc00fb53040592feeddc49dff207c0f70c59d
Triston Tetley <tetleylax@aol.com>
2019-11-12 02:54:02 -0500

    able to access spotify API and use it to console.log() the artist, name of the sone, and preview link

--- liri.js
+++ liri.js
@@ -1,19 +1,37 @@
 require("dotenv").config();
 
+var Spotify = require('node-spotify-api');
+
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+var spotify = (keys.spotify);
 
 var spotifyId = new Spotify({
-    id: spotify.process.env.SPOTIFY_ID,
-    secret: spotify.process.env.SPOTIFY_SECRET
+    id: spotify.id,
+    secret: spotify.secret
 });
 
 const command = process.argv[2];
+const song = process.argv[3];
 
 
 switch (command) {
     case ("spotify-this-song"):
-        console.log("maybe next time");
+        spotifyAPI();
         break;
 }
+
+
+function spotifyAPI() {
+    spotifyId.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+        console.log(JSON.stringify(data.tracks.items[0].name, null, 2));
+        console.log(JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 2));
+    });
+}
+
+
