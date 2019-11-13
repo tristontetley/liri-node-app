520b0508ebbbe67c373258074179f524fe7b1a05
Triston Tetley <tetleylax@aol.com>
2019-11-13 09:56:41 -0500

    have all the commands working in my terminal with current code

--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 
+const fs = require("fs");
+
+const axios = require("axios");
+
+var moment = require("moment");
+
 var Spotify = require('node-spotify-api');
 
 var keys = require("./keys.js");
@@ -12,27 +18,80 @@ var spotifyId = new Spotify({
 });
 
 const command = process.argv[2];
-const song = process.argv[3].split(" ").join("+");
+const search = process.argv.slice(3).join(" ");
 
-
-switch (command) {
-    case ("spotify-this-song"):
-        spotifyAPI();
-        break;
+function switchCase(command, search) {
+    switch (command) {
+        case ("spotify-this-song"):
+            spotifyAPI(search);
+            break;
+        case ("concert-this"):
+            seatGeekAPI(search);
+            break;
+        case ("movie-this"):
+            ombdAPI(search);
+            break;
+        case ("do-what-it-says"):
+            everything(search);
+            break;
+    }
 }
 
-
-function spotifyAPI() {
-    spotifyId.search({ type: 'track', query: song }, function (err, data) {
+function spotifyAPI(search) {
+    spotifyId.search({ type: 'track', query: search }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        
+
         console.log(JSON.stringify(data.tracks.items[0].name, null, 2));
         console.log(JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
         console.log(JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 2));
         console.log(JSON.stringify(data.tracks.items[0].album.name, null, 2));
     });
 }
+
+function seatGeekAPI(search) {
+    axios.get(`https://api.seatgeek.com/2/events?performers.slug=${search}&client_id=MTk0MzQzMDJ8MTU3MzU5NDE2OS42OA`)
+        .then(function (data) {
+            console.log(data.data.events[0].venue.name);
+            console.log(data.data.events[0].venue.extended_address);
+            let day = moment(data.data.events[0].datetime_utc).format("MM/DD/YYYY");
+            console.log(day);
+        })
+        .catch(function (error) {
+
+            console.log(error);
+        })
+}
+
+function ombdAPI(search) {
+    search = search || "Mr. Nobody"
+    axios.get(`http://www.omdbapi.com/?t=${search}&y=&plot=short&apikey=trilogy`).then(
+        function (response) {
+            console.log(response.data.Title);
+            console.log(response.data.Released);
+            console.log("The movie's rating is: " + response.data.imdbRating);
+            console.log(response.data.Ratings[1].Source);
+            console.log(response.data.Ratings[1].Value);
+            console.log("This movie was produced in " + response.data.Country);
+            console.log(response.data.Language);
+            console.log(response.data.Actors);
+        })
+        .catch(function (error) {
+
+            console.log(error);
+        })
+}
+
+function everything(command, search) {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = data.split(",");
+        switchCase(data[0], data[1]);
+    });
+}
+switchCase(command, search);
 
 
